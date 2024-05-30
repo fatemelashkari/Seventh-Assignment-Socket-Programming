586ffff1157b403f14a472bb4dfded621c5ab56f
Fateme Lashkari <fatemelashkariii84@gamil.com>
2024-05-30 15:01:59 +0330

    I completed the server and client part for messaging but I got an error and I con not run it now

--- seventh_assignment/src/main/java/Client/Client.java
+++ seventh_assignment/src/main/java/Client/Client.java
@@ -1,7 +1,7 @@
 package Client;
-import java.*;
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 // Client Class
 public class Client {
@@ -9,7 +9,7 @@ public class Client {
 
     // TODO: Add constructor and necessary methods
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: Implement the main method to start the client
         Socket socket = null;
         BufferedWriter bufferedwriter = null;
@@ -17,11 +17,43 @@ public class Client {
         OutputStreamWriter outputstreamWriter = null;
         InputStreamReader inputStreamReader = null;
         try{
-            socket = new Socket();//we have to
+            socket = new Socket("localhost" , 1234);//we have to pass two essential things to the Socket : 1.IP address 2. TCP port
+            inputStreamReader = new InputStreamReader(socket.getInputStream()); // read from the server
+            outputstreamWriter = new OutputStreamWriter(socket.getOutputStream()); // output from the server
+            bufferedreader = new BufferedReader(inputStreamReader);
+            bufferedwriter = new BufferedWriter(outputstreamWriter);
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.print("Type Your Message Here : ");
+                String message = scanner.nextLine();
+                bufferedwriter.write(message);
+                bufferedwriter.newLine();
+                bufferedwriter.flush();
+                System.out.println("The answer of the server : " + bufferedreader.readLine());
+                System.out.println("If you want to leave type quit");
+                if (message.equals("QUIT") || message.equals("quit")) {
+                    break;
+                }
+            }
         }catch (IOException e) {
             e.printStackTrace();
         }
         finally {
+            if (socket != null) {
+                socket.close();
+            }
+            if (bufferedwriter != null) {
+                bufferedwriter.close();
+            }
+            if (bufferedreader != null) {
+                bufferedreader.close();
+            }
+            if (outputstreamWriter != null) {
+                outputstreamWriter.close();
+            }
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
 
         }
     }
--- seventh_assignment/src/main/java/Server/Server.java
+++ seventh_assignment/src/main/java/Server/Server.java
@@ -1,12 +1,46 @@
 package Server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 // Server Class
 public class Server {
     // TODO: Implement the server-side operations
 
     // TODO: Add constructor and necessary methods
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: Implement the main method to start the server
+        Socket socket = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        ServerSocket serverSocket = null;
+        serverSocket = new ServerSocket(1234);
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
+                bufferedReader = new BufferedReader(inputStreamReader);
+                 while (true) {
+                     String message = bufferedReader.readLine();
+                     System.out.println("The Client said : " + message);
+                     if (message.equals("Quit")) {
+                         break;
+                     }
+                 }
+                socket.close();
+                bufferedWriter.close();
+                bufferedReader.close();
+                inputStreamReader.close();
+                outputStreamWriter.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
