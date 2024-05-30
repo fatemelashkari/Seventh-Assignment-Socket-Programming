f3a35694b07bd23738ca0ec951db95a2bf613a73
Fateme Lashkari <fatemelashkariii84@gamil.com>
2024-05-30 12:24:16 +0330

    first I had a problem with the intellij to run the code then I asked for help then I solve this problem (I couldn't run my code, when I put the client and the server package inside the java directory which I creates inside the main the problem solved)

---
+++ seventh_assignment/src/main/java/Client/Client.java
@@ -0,0 +1,28 @@
+package Client;
+import java.*;
+import java.io.*;
+import java.net.Socket;
+
+// Client Class
+public class Client {
+    // TODO: Implement the client-side operations
+
+    // TODO: Add constructor and necessary methods
+
+    public static void main(String[] args) {
+        // TODO: Implement the main method to start the client
+        Socket socket = null;
+        BufferedWriter bufferedwriter = null;
+        BufferedReader bufferedreader = null;
+        OutputStreamWriter outputstreamWriter = null;
+        InputStreamReader inputStreamReader = null;
+        try{
+            socket = new Socket();//we have to
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+
+        }
+    }
+}
